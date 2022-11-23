28efc2506c680ff27f43b5cd7b70aa3cf0fe19c3
Modasserul Hoque <modasserulhaq.jasim@gmail.com>
2022-11-24 04:44:54 +0600

    Basic server setup completed

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+require('colors');
+
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+//Middle wares
+app.use(cors());
+app.use(express.json());
+
+// configure MongoDB
+
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const usersCollection = client.db('computerBazar').collection('users');
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('Database Connected'.yellow);
+    } catch (error) {
+        console.log(error.name.bgRed, error.message.bold, error.stack);
+    }
+}
+run();
+
+app.get('/', (req, res) => {
+    res.send("Computer Bazar Server is Running");
+})
+
+app.listen(port, () => {
+    console.log(`Computer Bazar is running on port ${port}`.cyan.bold);
+})
